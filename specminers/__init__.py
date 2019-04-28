b9571f3d61d01ca934e4466d7f48562b761e24ba
ChrisTimperley <christimperley@googlemail.com>
2019-04-28 14:21:10 -0400

    added MiningTool

--- specminers/__init__.py
+++ specminers/__init__.py
@@ -3,3 +3,21 @@
 specminers provides a Python wrapper around several popular specification
 mining tools.
 """
+import docker
+import attr
+
+
+class MiningTool:
+    pass
+
+
+@attr.s(frozen=True)
+class Daikon(MiningTool):
+    image: str = attr.ib(default='christimperley/daikon')
+
+    def __call__(self, *filenames: str) -> None:
+        # TODO copy each file into the container
+        # TODO compose command
+        # TODO call command
+        # TODO it's probably easier to just create a container per command
+        return
