5eda03eba803211a81c9784541fcb6e1b5c7b40b
Chris Timperley <christimperley@googlemail.com>
2019-04-29 13:46:47 -0400

    Added basic output parsing to __call__ for Daikon (#10)

--- specminers/__init__.py
+++ specminers/__init__.py
@@ -4,6 +4,7 @@ specminers provides a Python wrapper around several popular specification
 mining tools.
 """
 import os
+import shlex
 
 import docker
 import attr
@@ -18,8 +19,10 @@ class Daikon(MiningTool):
     client = attr.ib(default=docker.from_env())
     image: str = attr.ib(default='christimperley/daikon')
 
-    def __call__(self, *filenames: str) -> None:
+    def __call__(self, *filenames: str) -> str:
+        """Executes the Daikon binary."""
         # TODO ensure given files exists
+        assert filenames, "expected one or more filenames."
 
         ctr_dir = '/tmp/.specminers'
         host_filenames = filenames
@@ -29,14 +32,21 @@ class Daikon(MiningTool):
         volumes = {fn_host: {'bind': fn_ctr, 'mode': 'ro'}
                    for fn_host, fn_ctr in host_to_ctr_fn.items()}
 
-        command = ' '.join(["java", "daikon.Daikon"] + ctr_filenames)
+        command = (
+            'java daikon.Daikon '
+            '--no_show_progress --no_text_output --noversion '
+            '-o /tmp/mined.inv.tgz '
+            f"{' '.join(shlex.quote(f) for f in ctr_filenames)} "
+            '&> /dev/null '
+            '&& java daikon.PrintInvariants /tmp/mined.inv.tgz')
+        command = f'/bin/bash -c "{command}"'
         output = self.client.containers.run(self.image, command,
-                                            volumes=volumes)
-
-        # TODO parse output
-        print(output)
+                                            volumes=volumes).decode('utf-8')
+        return output
 
 
 if __name__ == '__main__':
     daikon = Daikon()
-    daikon('cool.dtrace')
+    filenames = ['/home/chris/tools/specminers.py/ardu.decls',
+                 '/home/chris/tools/specminers.py/ardu.dtrace']
+    daikon(*filenames)
