bfbe1e76da2dd081a13661d05464552e0a00dd91
ChrisTimperley <christimperley@googlemail.com>
2020-04-10 10:23:06 -0400

    moved deps to setup.cfg

--- setup.py
+++ setup.py
@@ -1,20 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import os
-from setuptools import setup
+import setuptools
 
-
-PACKAGE_NAME = 'specminers'
-VERSION = '0.0.1'
-
-setup(
-    name=PACKAGE_NAME,
-    version=VERSION,
-    python_requires='>=3.5',
-    install_requires=[
-        'attrs>=17.2.0',
-        'typing-extensions>=3.7.2',
-        'docker~=3.7.2'
-    ],
-    packages=['specminers']
-)
+setuptools.setup(version='0.0.1')
